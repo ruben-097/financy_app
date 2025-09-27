f2c11f9d48be42ce4687668ac9f510c98a197d43
ruben-097 <rubenmaimona19@gmail.com>
2025-09-26 23:30:54 -0300

    atualização app

---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const List<Color> gradientPurple = [
+    Color(0xFF360253),
+    Color(0xFF2E0249),
+  ];
+
+  static const whiteText = Color(0xFFFFFFFF);
+}
---
+++ lib/features/splash/splash_page.dart
@@ -0,0 +1,30 @@
+import 'package:financy_app/common/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: AppColors.gradientPurple,
+          ),
+        ),
+        child: const Text(
+          'Financy',
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            color: AppColors.whiteText,
+          ),
+        ),
+      ),
+    );
+  }
+}
